c83455d39b2c075bcb23ad6f77fd2e05e9260ac7
Saketh Gunnam <gunnamsrisaketh@gmail.com>
2024-05-15 14:41:44 +0530

    Revert "git first push"
    This reverts commit 5d2233ebbde92100eaff20fe403bb78d27c486b3.

--- src/main/java/com/example/StudentDetails/service/OpenApiConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.StudentDetails.service;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-@Configuration
-public class OpenApiConfig implements WebMvcConfigurer {
-}
